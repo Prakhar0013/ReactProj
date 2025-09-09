247a6b9234007dd6aa266b91ff999fb7d4b6c995
Prakhar <prakharagrawal0013@gmail.com>
2025-09-09 11:20:38 +0530

    Custom React and React functions

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import MyPage from './MyPage'
 function App() {
+  const username= "Prakhar"
 
   return (
+    <>
     <MyPage/>
-
+    <h1>Project</h1>
+    <p>Username: {username}</p>
+    </>
   )
 }
 
